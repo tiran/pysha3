ae2ae6c996055c6494e40ef56485ce5099994900
Christian Heimes <christian@python.org>
2012-10-04 01:53:48 +0200

    tune setup.py to support 32 and 64bit platforms

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
+import sysconfig
 from distutils.core import setup
 from distutils.extension import Extension
+from glob import glob
 
-setup_info = dict(
+pointer_size = sysconfig.get_config_var('SIZEOF_VOID_P') * 8
+
+sha3_files = ["Modules/sha3module.c",
+              "Modules/keccak/KeccakNISTInterface.c",
+              "Modules/keccak/KeccakSponge.c"]
+if pointer_size == 32:
+    sha3_files.append("Modules/keccak/KeccakF-1600-opt32.c")
+elif pointer_size == 64:
+    sha3_files.append("Modules/keccak/KeccakF-1600-opt64.c")
+else:
+    raise ValueError(pointer_size)
+
+sha3_depends = glob("Modules/keccak/*.h") + glob("Modules/keccak/*.macros")
+
+
+setup(
     name="pykeccak",
     version="0.1",
     ext_modules=[
-        Extension("_sha3",
-                  ["Modules/sha3module.c",
-                   "Modules/keccak/KeccakNISTInterface.c",
-                   "Modules/keccak/KeccakSponge.c",
-                   "Modules/keccak/KeccakF-1600-opt64.c",
-                   ]),
+        Extension("_sha3", sha3_files, depends=sha3_depends)
         ],
     author="Christian Heimes",
     author_email="christian@python.org",
@@ -31,5 +43,3 @@ setup_info = dict(
         'Programming Language :: C',
     ),
 )
-
-setup(**setup_info)
