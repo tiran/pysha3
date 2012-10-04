01cf48a5d00fb4dd42133a28f4b81da1064f185f
Christian Heimes <christian@cheimes.de>
2012-10-04 11:36:41 +0200

    add fascade module

---
+++ sha3.py
@@ -0,0 +1,7 @@
+#  Copyright (C) 2012   Christian Heimes (christian@python.org)
+#  Licensed to PSF under a Contributor Agreement.
+#
+
+from _sha3 import sha3_224, sha3_256, sha3_384, sha3_512
+sha3 = sha3_512
+
