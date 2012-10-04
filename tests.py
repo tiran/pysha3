43739ce3cb131a0df2fb1f762f2cdefc061ca956
Christian Heimes <christian@cheimes.de>
2012-10-04 16:14:01 +0200

    set exit code after tests

--- tests.py
+++ tests.py
@@ -131,4 +131,5 @@ def test_main():
     return suite
 
 if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(test_main())
+    result = unittest.TextTestRunner(verbosity=2).run(test_main())
+    sys.exit(not result.wasSuccessful())
