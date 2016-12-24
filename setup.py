13c695fca0f5e05eba33f296edb66e5bd23b761f
Christian Heimes <christian@python.org>
2016-12-24 09:12:26 +0100

    Prepare release 1.0.0
    Signed-off-by: Christian Heimes <christian@python.org>

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ with open("CHANGES.txt") as f:
 
 setup(
     name="pysha3",
-    version="1.0.dev2",
+    version="1.0.0",
     ext_modules=exts,
     py_modules=["sha3"],
     cmdclass={"test": TestCommand},
